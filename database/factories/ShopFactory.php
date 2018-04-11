d1053a1eb3f96eb7e4e8ef6213c2bb02352bce6b
natthapach <natthapach.a@ku.th>
2018-04-11 16:07:17 +0700

    complete shop factory

--- database/factories/ShopFactory.php
+++ database/factories/ShopFactory.php
@@ -4,19 +4,20 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Shop::class, function (Faker $faker) {
     $shop_name = $faker->unique()->word();
-    $owner_id = $faker->randomElement(
-        App\User::where("role", "owner")->pluck("id")->toArray()
+    $owner = $faker->randomElement(
+        App\User::owner()->get()->toArray()
     );
-    // $create_time = 
+    $create_time = $faker->dateTimeBetween($startDate=$owner["created_at"]);
     return [
         "name" => $shop_name,
-        "owner_id" => 
+        "owner_id" => $owner["id"],
         "phone" => $faker->phoneNumber,
         "email" => $shop_name . "@example.com",
         "category" => $faker->randomElement([
             'restaurant','cafe','salon','fitness','mall','cinema'
         ]),
         "logo_img" => $shop_name . "-logo.jpg",
-
+        "created_at" => $create_time,
+        "updated_at" => $create_time
     ];
 });
