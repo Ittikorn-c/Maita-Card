4d203bd0729bcd89d651f24b512db0ce64fd9274
natthapach <natthapach.a@ku.th>
2018-04-12 09:41:56 +0700

    complete CardTemplate factory

---
+++ database/factories/CardTemplateFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\CardTemplate::class, function (Faker $faker) {
+    $shop = $faker->randomElement(
+        App\Shop::all()->toArray()
+    );
+    $created_at = $faker->dateTimeBetween($min=$shop["created_at"]);
+    return [
+        "shop_id" => $shop["id"],
+        "name" => $faker->word,
+        "img" => "sample-cardtemplate.jpg",
+        "style" => $faker->randomElement([
+            "stamp", "point"
+        ]),
+        "created_at" => $created_at,
+        "updated_at" => $created_at
+    ];
+});
