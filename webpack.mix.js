e6f6dd206863fa2de49bf8577724755f32c7f6a5
natthapach <natthapach.a@ku.th>
2018-04-26 18:04:58 +0700

    space bar

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,7 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+ 
 mix.js('resources/assets/js/app.js', 'public/js')
     .js('resources/assets/js/register.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
