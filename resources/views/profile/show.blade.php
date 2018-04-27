92a720e30ed5a3ff96e9f7102ba1f1e4ba8066c8
Chouligen <36153901+Chouligen@users.noreply.github.com>
2018-04-28 01:29:34 +0700

    Profile Show update
    update row col profile show

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -7,14 +7,25 @@ User Profile
 @section('content')
 <div class="panel panel-default">
     <div class="panel-heading">
-        <h2>{{ $user->username }}</h2>
+        <h2>User Profile : {{ $user->username }}</h2>
     </div>
-    <ul class="list-group">
-      <li class="list-group-item">First Name: {{ $user->fname }}</li>
-      <li class="list-group-item">Email: {{ $user->email }}</li>
-      <li class="list-group-item">
-        Joining Date: {{ $user->created_at->diffForHumans() }}
-      </li>
-    </ul>
+    <div class="row m-3 p-2">
+        <div class="col-sm-1">
+            
+        </div>
+        <div class="col-sm-4">
+            Pic
+        </div>
+        <div class="col-sm-6">
+            <ul class="list-group">
+                <li class="list-group-item">First Name: {{ $user->fname }}</li>
+                <li class="list-group-item">Last Name: {{ $user->lname }}</li>
+                <li class="list-group-item">Email: {{ $user->email }}</li>
+                <li class="list-group-item">Address: {{ $user->address }}</li>
+                <li class="list-group-item">Joining Date: {{ $user->created_at->diffForHumans() }}</li>
+            </ul>
+        </div>
+    </div>
 </div>
+
 @endsection
