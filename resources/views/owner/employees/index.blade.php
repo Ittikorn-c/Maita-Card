d0bcf99b05d93076a54dd03877aa8fdc2029843a
Chouligen <36153901+Chouligen@users.noreply.github.com>
2018-04-30 03:56:50 +0700

    employees update
    table

--- resources/views/owner/employees/index.blade.php
+++ resources/views/owner/employees/index.blade.php
@@ -7,19 +7,32 @@
       <th scope="col">#</th>
       <th scope="col">Username</th>
       <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Shop Name</th>
+      <th scope="col">Branch Name</th>
     </tr>
   </thead>
   <tbody>
+    
     @foreach($employees as $employee)
+    @if(Auth::user()->id === $employee->branch->shop->owner_id)
     <tr class='table'>
       <th scope="row">{{ $loop->iteration }}</th>
       <td>
         <a href="{{ url('/profile/' . $employee->user_id) }}">
-          {{ $employee->username }}
+          {{ $employee->user->username }}
         </a>
       </td>
-      <td>{{ $employee->fname }}</td>
+      <td>{{ $employee->user->fname }}</td>
+      <td>{{ $employee->user->lname }}</td>
+      <td>{{ $employee->user->email }}</td>
+      <td>{{ $employee->user->phone }}</td>
+      <td>{{ $employee->branch->shop->name }}</td>
+      <td>{{ $employee->branch->name }}</td>
     </tr>
+    @endif
     @endforeach
   </tbody>
 </table>
