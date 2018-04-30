3b710723a5be68205414baa7449d640a2463a85f
natthapach <natthapach.a@ku.th>
2018-04-30 13:09:31 +0700

    remove my cards menu again

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,9 +81,6 @@
                                     <a class="dropdown-item" href='{{ url("/" . Auth::user()->id . "/scan") }}'>
                                         Check in
                                     </a>
-                                    <a class="dropdown-item" href='{{ url("") }}'>
-                                        My Cards
-                                    </a>
                                     @elseif(Auth::user()->role === "owner")
                                     <a class="dropdown-item" href='{{ url("/profile/" . Auth::user()->id) }}'>
                                         Profile
